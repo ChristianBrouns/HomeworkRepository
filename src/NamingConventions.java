ff450ced9444e37dcd315fb01f89428844ae26c3
Christian Brouns <christian.brouns@gmail.com>
2016-01-05 17:19:22 +0100

    Naming Conventions
    naming conventions in Java

---
+++ src/NamingConventions.java
@@ -0,0 +1,45 @@
+/**
+ * Created by Christian on 5-1-2016.
+ */
+public class NamingConventions {
+    int isChanged = 15;
+    String $name = "Christian";
+    double €name = 15.76;
+    String Name = "Christian";
+    int string = 8;
+    double For = 4.90;
+    String While = "Longtime";
+    int _adress = 21;
+    String __444 = "Numbers";
+
+    int 5name = "2";
+    String #name = "Someone";
+    double for = 1.25;
+    int while = 5;
+    boolean switch = true;
+}
+
+
+
+/*
+Toegestane variabele namen   
+De volgende zijn toegestane variabelen namen …   
+ isChanged 
+ $name 
+ €name 
+ Name 
+ string 
+ For 
+ While 
+ _address 
+ __4444 
+  
+ De volgende zijn niet toegestaan….   
+ 5name 
+ #name 
+ for 
+ while 
+ switch 
+
+Schrijf een java class om uit te proberen de toegestande variabelen en lees het stukje ‘Naming’  in ​Naming conventions 
+ */
