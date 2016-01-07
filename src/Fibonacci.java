45a1b7d28e0d8725b7c0a55807b7df3cdbbd452b
Christian Brouns <christian.brouns@gmail.com>
2016-01-07 15:26:01 +0100

    homework 07-1-2016
    Complete homework

--- src/Fibonacci.java
+++ src/Fibonacci.java
@@ -2,9 +2,20 @@
  * Created by Christian on 5-1-2016.
  */
 public class Fibonacci {
+
+   public static int fibonacci(int number) {
+        if ((number == 0) || (number == 1))
+            return number;
+        else
+            return fibonacci(number - 1) + fibonacci(number - 2);
+    }
+    public static void main(String[] args) {
+        int input = 20;
+        for (int counter = 0; counter <= input; counter++){
+            System.out.print(" " + fibonacci(counter));
+        }
+    }
 }
-
-
 /*
 
 Fibonacci sequence  
